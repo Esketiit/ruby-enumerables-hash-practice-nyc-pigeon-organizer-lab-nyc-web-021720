fb954ed97c8d4f6093af9786573a44290954e6cf
Brian Desplantes <heftygac@gmail.com>
2020-02-19 13:58:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	organized_data = {}
+
+	#create the top layer of organized_data
+	#thinking that I can create the sub layers using key, value variables
+	data.each do |key, value|
+		puts key
+		value.each do |k, v|
+			v.each do |name|
+				if !organized_data[name.to_sym]
+					organized_data[name.to_sym] = {}
+				end
+			end
+		end
+	end
+
+	return organized_data
 end
